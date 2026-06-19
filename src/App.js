477ce13f301be69193e5f21bc17f4755d08cad1f
ponting12 <adityayadav12114@gmail.com>
2026-06-20 02:16:43 +0530

    adding dark theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+//import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <div className="container" >
-        <TextForm heading = "Enter the text to analyze below:"/>
+        {/* <TextForm heading = "Enter the text to analyze below:"/> */}
+        <About></About>
       </div>
       
     </div>
