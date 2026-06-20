947e4a7dab6b270bf1aa944237760666fc515931
ponting12 <adityayadav12114@gmail.com>
2026-06-21 02:03:49 +0530

    adding router not complerte yet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React,{useState} from "react";
 import Navbar from "./components/Navbar";
-//import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 import About from "./components/About";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <div className="container" >
-        {/* <TextForm heading = "Enter the text to analyze below:"/> */}
-        <About></About>
+         <TextForm heading = "Enter the text to analyze below:"/> 
+         <About></About> 
       </div>
       
     </div>
