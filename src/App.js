ad7a086c2ff96771820b84e3407b338f595ac031
ponting12 <adityayadav12114@gmail.com>
2026-06-22 01:33:35 +0530

    react router is added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React,{useState} from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <div className="container" >
-         <TextForm heading = "Enter the text to analyze below:"/> 
-         <About></About> 
+    <Router>
+      <Navbar />
+
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={<TextForm heading="Enter the text to analyze below:" />}
+          />
+
+          <Route
+            path="/about"
+            element={<About />}
+          />
+        </Routes>
       </div>
-      
-    </div>
+    </Router>
   );
 }
+
 export default App;
