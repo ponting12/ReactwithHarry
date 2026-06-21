8b186bb7dc10e7dc92bf34c08674d6a7d0dbfd02
ponting12 <adityayadav12114@gmail.com>
2026-06-22 02:00:53 +0530

    last commit for today

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg  navbar-dark bg-dark ">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            Text Utiles
+            Text Analyzer 
           </Link>
           <button
             className="navbar-toggler"
